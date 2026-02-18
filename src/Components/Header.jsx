1ac796d3deebe428a1e6ad035de3bdf16b6f1257
Nour Muhsen <nourmuhsen2002@hotmail.com>
2026-02-18 22:11:01 +0300

    Refactor Header component with responsive navigation
    Added state management for hamburger menu toggle and improved navigation structure.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,28 +1,60 @@
-import { Link, NavLink } from "react-router-dom"
-import reactLogo from '../Assets/react.svg'
-import "./Header.css"
+import { useState } from 'react';
+import { NavLink } from "react-router-dom";
+import reactLogo from '../Assets/react.svg'; // Fixed capitalization
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <header className="d-flex flex-row align-items-center p-3 
-                  px-4 mb-3 border-bottom border-primary header">
-            <div className="d-flex flex-row align-items-center my-2 me-auto me-auto">
-                <img className="reactLogo me-2"
-                    src={reactLogo} alt="React Logo"
-                />
-                <h5 className="fs-4 m-0 text-white">
-                    <NavLink className="Link" to="/">GymLog</NavLink>
-                </h5>
-            </div>
+    // State to toggle the hamburger menu
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
 
-            <nav className="my-2  me-3">
-                <NavLink className="p-2 Link" to="/create-routine">Create a Routine</NavLink>    
-                <NavLink className="p-2 Link" to="/current-routine">Current Routine</NavLink>
-                <NavLink className="p-2 Link" to="/my-routines">My Routines</NavLink>
-                <NavLink className="p-2 Link" to="/support">Support us</NavLink>
-            </nav>
-        </header>
-    )
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+    const closeMenu = () => setIsNavCollapsed(true);
+
+    return (
+        // navbar-expand-md: Menu shows fully on Medium screens (tablets) and up
+        // navbar-dark: Tells Bootstrap we have a dark background, so text/icons should be white
+        <nav className="navbar navbar-expand-md navbar-dark border-bottom border-primary header px-4">
+            <div className="container-fluid">
+                
+                {/* Brand / Logo Section */}
+                <div className="navbar-brand d-flex align-items-center">
+                    <img className="reactLogo me-2" src={reactLogo} alt="React Logo" />
+                    <h5 className="fs-4 m-0">
+                        <NavLink className="Link" to="/" onClick={closeMenu}>GymLog</NavLink>
+                    </h5>
+                </div>
+
+                {/* Hamburger Button (Visible only on mobile) */}
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    onClick={handleNavCollapse}
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                {/* Collapsible Menu Links */}
+                {/* The 'collapse' class hides the menu when isNavCollapsed is true */}
+                <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
+                    <div className="navbar-nav ms-auto text-center">
+                        <NavLink className="nav-link Link p-2" to="/create-routine" onClick={closeMenu}>
+                            Create a Routine
+                        </NavLink>    
+                        <NavLink className="nav-link Link p-2" to="/current-routine" onClick={closeMenu}>
+                            Current Routine
+                        </NavLink>
+                        <NavLink className="nav-link Link p-2" to="/my-routines" onClick={closeMenu}>
+                            My Routines
+                        </NavLink>
+                        <NavLink className="nav-link Link p-2" to="/support" onClick={closeMenu}>
+                            Support us
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default Header
+export default Header; 
